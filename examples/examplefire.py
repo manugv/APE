c377aa66353e694f945ffd6da5af29f97fcb73dd
Manu  Goudar Vishwanathappa <manugv@libra.sron.nl>
2024-03-05 20:30:45 +0100

    Added examples for fires.

---
+++ examples/examplefire.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(1, "/data/manugv/github/APE")
+
+from APE.ModuleInitialParameters import InputParameters
+import APE.Fire as iape
+from APE.ModCheckDownloadVelocity import download_modelleveldata
+from APE.ModuleInjectionHeight import download_injectionheight, InjectionHeight
+
+filename = "/data/manugv/github/APE/examples/fire_example.yaml"
+
+params = InputParameters(filename)
+
+_day = params.days[0]
+
+# plume detection
+nogoodplumes= iape.datapreparation_plumedetection(params.days[0], params)
+
+# check/download velocity data
+inputfilename = params.output_file_prefix + _day.strftime("%Y_%m")
+download_modelleveldata(inputfilename, params.estimateemission.flow, _day,
+                        params.estimateemission.flow.cdsapikey, params.estimateemission.flow.cdsapikey)
+
+# check/download injection height data
+emis = params.estimateemission
+download_injectionheight(emis.injht_dir, _day, emis.injht_adsapiurl, emis.injht_adsapikey)
+
+iape.emissionestimationfires(_day, params)
+
